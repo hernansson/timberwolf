87973bc48c23c512c861d853a45170e23ed967ab
Hernan Suzuki Son <hernan.ss@live.com>
2022-09-21 12:44:16 -0300

    added navigation

--- src/pages/Home/components/RegisterContainer.jsx
+++ src/pages/Home/components/RegisterContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { RegisterForm } from './RegisterForm';
+import { useNavigate } from 'react-router-dom';
 export const RegisterContainer = () => {
   //useHook with some isLoading / is error logic.
-
+  const navigate = useNavigate();
   const handleAsync = data => {
     console.log(data);
+    navigate('/confirmation');
   };
   return (
     <div>
