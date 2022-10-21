f6be27c316a4f1e65060f0306663008ccb82713f
Hernan Suzuki Son <hernan.ss@live.com>
2022-10-21 20:25:57 -0300

    tsdt

--- src/pages/Home/components/ModalStepper/styles.js
+++ src/pages/Home/components/ModalStepper/styles.js
@@ -2,8 +2,9 @@ import { isIOS } from '../../../../utils/isIOS';
 
 export const styles = {
   mainContainer: {
-    background: 'rgba( 255, 255, 255, 0.1 )',
-    boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+    background: isIOS()
+      ? 'rgba( 100, 100, 100, 0.6 )'
+      : 'rgba( 255, 255, 255, 0.1 )',
     backdropFilter: 'blur(50.5px)',
     '-webkit-backdrop-filter': 'blur(50.5px)',
     borderRadius: '30px',
